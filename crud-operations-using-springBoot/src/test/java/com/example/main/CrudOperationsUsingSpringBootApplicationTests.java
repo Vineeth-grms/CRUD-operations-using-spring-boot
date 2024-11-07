6f4ac4b92d311bfbe493a7b2a79f9a6ffe1c051d
Vineeth-grms <gumma272@gmail.com>
2024-11-07 11:11:15 +0530

    I have done JUNIT test case for addEmployee Method.

--- crud-operations-using-springBoot/src/test/java/com/example/main/CrudOperationsUsingSpringBootApplicationTests.java
+++ crud-operations-using-springBoot/src/test/java/com/example/main/CrudOperationsUsingSpringBootApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.main;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.model.Employee;
+import com.example.repository.EmployeeRepository;
 
 @SpringBootTest
 class CrudOperationsUsingSpringBootApplicationTests {
 
+	@Autowired
+	EmployeeRepository repository;
+
 	@Test
-	void contextLoads() {
+	public void testAddEmployee() {
+		Employee emp = new Employee();
+		emp.setEmpName("MS Dhoni");
+		emp.setSalary(5000);
+		emp.setAddress("Ranchi");
+		emp.setDept("Coaching");
+		Employee savedEmployee = repository.save(emp);
+		assertNotNull(repository.findById(savedEmployee.getEmpId()).get());
+
 	}
 
 }
